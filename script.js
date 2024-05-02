3a5a83ebc06d6fd4064749dc10ea898080e92cc5
Aman kumar <amank0259@gmail.com>
2024-05-02 11:07:52 +0530

    make the functional click start button tom start and click on stop to stop the car

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 var audio = document.createElement("audio");
 document.body.appendChild(audio);
-audio.src="caraudio.mp3"
+audio.src = "caraudio.mp3";
+
+var roadanim = document.querySelector('.road');
+var caranim = document.querySelector('.car');
+var wheel1anim = document.querySelector('.wheel1');
+var wheel2anim = document.querySelector('.wheel2');
 
 
-document.body.addEventListener("mousemove", function(){
-    audio.play();
-    audio.loop();
-})
+function start() {
+    audio.play()
+    audio.loop = true;
+    roadanim.classList.add('roadstart');
+    caranim.classList.add('carstart');
+    wheel1anim.classList.add('wheel1start');
+    wheel2anim.classList.add('wheel2start');
+}
+
+function stop() {
+    audio.pause()
+    roadanim.classList.remove('roadstart');
+    caranim.classList.remove('carstart');
+    wheel1anim.classList.remove('wheel1start');
+    wheel2anim.classList.remove('wheel2start');
+}
